550400b444d54b686e30954b0900af214dcd8013
dlinkoln <dlinkoln99@gmail.com>
2021-02-18 11:49:03 +0200

    init tpl with vuex and scss

---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Some stuff</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+body {
+  div {
+    background: red;
+  }
+}
+</style>
